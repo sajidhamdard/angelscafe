1d1f7a87f22512e3e29b3e419c68f16a344f6e1f
Sajid Khan <sajid.hmdrd@gmail.com>
2023-07-17 08:35:37 +0530

    Made some changes

--- myscript.js
+++ myscript.js
@@ -1,1 +1,6 @@
-Hello Nodejs
+const events = require("events");
+const myEvent = new events.EventEmitter();
+myEvent.on("feeDeposit", function (name, id) {
+  console.log(`Fees is depositd by ${name} and id : ${id}`);
+});
+myEvent.emit("feeDeposit", "Sajid", 10);
